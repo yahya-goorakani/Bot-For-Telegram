16dd78c95b472d44464a4ac860adc1d366d6ae09
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2024-05-04 04:20:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,3 +30,26 @@ if __name__ == "__main__":
 
 
 
+
+
+
+
+# To find ID_chat
+# import requests
+# TOKEN = "***"
+# url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+# print(requests.get(url).json())
+
+
+# To seand message
+# import requests
+# TOKEN = "***"
+# # chat_id = "****"
+# message = "hello from your telegram bot"
+# url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+# print(requests.get(url).json()) # this sends the message
+
+
+
+
+
